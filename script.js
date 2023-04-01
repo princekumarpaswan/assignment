ad566721fb46813e074c475597dd68e1385b7530
princekumarpaswan <prasantandal123@gmail.com>
2023-04-01 21:47:32 +0530

    done no bug and issue

--- script.js
+++ script.js
@@ -1,80 +1,66 @@
-// add note toggle
-let count = 1
-let subcount = 2
 let tar = false
-let title = document.getElementById("titleText")
-let note = document.getElementById("mainNote")
-let notesElem = document.querySelector(".cointainer")
-let toggle = true
-const newNote = () => {
-    let showInput = document.getElementById("input")
-
-    if (toggle) {
-        showInput.style = "display:block"
-        toggle = false
-    } else {
-        showInput.style = "display:hiden"
-        toggle = true
-    }
-}
-
+let count = 1
+let hh = 1
 let submit = document.getElementById("btn");
 submit.addEventListener("click", (e) => {
     e.preventDefault();
     addNotes()
 })
+let notsElem = document.querySelector(".cointainer")
+
 
 function addNotes() {
-    let titleValue = title.value;
-    let noteValue = note.value
+
     let card = document.createElement('div')
+    console.log(card);
     card.classList.add("card")
+    card.id = `${"z" + count}`
     document.querySelector(".card")
-    if (titleValue && noteValue) {
-        card.innerHTML = `
-    <div class="cardflex">
+    card.innerHTML = `
+        <div class="cardflex">
+        <div class="title-div">
+            <input type="text"  placeholder="Enter Title">
+            <div onclick="show(this.id)" id="${count}" class="dotbtn"><i class="fas fa-ellipsis-v"></i></div>
+        </div>
+        <textarea placeholder="Enter Note" cols="30" rows="7"></textarea>
+            </div>
+            <div id=${"a" + count} class="cardColo">
+                <h5 class="coloHead">Change Background</h5>
                 <div>
-                    <h3>${titleValue}</h3>
+                    <div value="#FFFFFF" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li1"><i  class="fa-solid fa-circle"></i></div>
+                    <div value="#CBD2D6" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li2"><i  class="fa-solid fa-circle"></i></div>
+                    <div value="#CCE8E4" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li3"><i  class="fa-solid fa-circle"></i></div>
+                    <div value="#FAF1DB" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li4"><i  class="fa-solid fa-circle"></i></div>
+                    <div value="#FDE9D9" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li5"><i  class="fa-solid fa-circle"></i></div>
+                    <div value="#F9DCD5" id="${"b" + hh++}" onclick="changeColo(this.id)" class="li6"><i  class="fa-solid fa-circle"></i></div>
                 </div>
-                <div>
-                    <h3 id="${count}" onclick="call(this.id)" class="dot"><i class="fa-solid fa-ellipsis-vertical"></i></h3>
-                </div>
+                <hr class="line">
+                <h4 class="delete">Delete</h4>
+            </div>
+        </div>
+        `
 
-                <div id="${count + 1}" class="cardColo">
-                    <h5 class="coloHead">Change Background</h5>
-                    <ul>
-                        <li class="li1"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                        <li class="li2"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                        <li class="li3"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                        <li class="li4"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                        <li class="li5"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                        <li class="li6"><i onclick="chanceCol(this.id)" id="${count}" class="fa-solid fa-circle"></i></li>
-                    </ul>
-                    <hr class="line">
-                    <h4 class="delete">Delete</h4>
-                </div>
-                <div class="list">
-                <textarea class="menu left" cols="20" rows="10"disabled>${noteValue}</textarea>
-                </div>
-            </div>`
-        titleValue = ""
-        noteValue = ""
-        notesElem.appendChild(card)
+    notsElem.appendChild(card)
+    count = count + 1
 
-        let showInput = document.getElementById("input")
-        showInput.style = "display:hiden"
-        toggle = true
-        count = count + 1
-        tar = true
-    } else {
-        alert("title and/or note missing")
-    }
-    console.log(card.querySelector(".delete"));
     let clear = card.querySelector(".delete")
     clear.addEventListener("click", () => card.remove())
 
 }
 
+function show(id) {
+    if (tar === false) {
+        const dotbtn = document.getElementById(`${"a" + id.toString()}`)
+        dotbtn.style = "display: block"
+        tar = true
+    } else {
+        const dotbtn = document.getElementById(`${"a" + id.toString()}`)
+        dotbtn.style = "display: none"
+        tar = false
+    }
+
+
+}
 
 
 function call(click_id) {
@@ -89,13 +75,15 @@ function call(click_id) {
 
 }
 
-function chanceCol(getId) {
-
-    const colours = ["#FFFFFF", "#CBD2D6", "#CCE8E4", "#FAF1DB", "#FDE9D9", "#F9DCD5"]
-
-    const colo = document.getElementById(`${"c" + getId.toString()}`)
-    console.log(colo);
-    const cards = document.getElementsByClassName("card")
-    cards[getId - 1].style.backgroundColor = `${colours[getId]}`
-
+function changeColo(value) {
+    const elem = document.getElementById(value.toString()).parentElement
+    const subelem = elem.parentElement
+    const mainelem = subelem.parentElement
+    const cul = mainelem.getAttribute('id')
+    const data = document.getElementById(value.toString()).getAttribute('value')
+    const mChange = document.getElementById(cul)
+    mChange.style.backgroundColor = data
 }
+
+
+
